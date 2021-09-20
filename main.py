961c533b7c4406d7c7d592f608eb0f7b6b28254c
Васильев Павел <40562751+vaspadi@users.noreply.github.com>
2021-09-20 06:31:32 +0000

    Create full name printing

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-erik = 'erik lox'
+firstName  = "Pavel"
+secondName = 'Vasiljev'
 
-print(erik)
+print('Your full name is ' + firstName + ' ' + secondName + '!')
+
+print('Your full name is', firstName, secondName)
+
+print("Your name is %a" %firstName)
+
+print('Your full name is {0} {1}' .format(firstName, secondName))
+
+print(f"Your full name is {firstName} {secondName}")
+
+print('1' + 1)
