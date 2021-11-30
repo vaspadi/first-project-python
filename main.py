36d15b8623c925f7bc3912a8e36b9c181d13cd26
Васильев Павел <40562751+vaspadi@users.noreply.github.com>
2021-11-30 07:22:33 +0000

    test PIL package

--- main.py
+++ main.py
@@ -1,43 +1,10 @@
-# class Info:
-#   def __init__(self):
-#     self.PVN = 21
-#     self.darbaSamaksa = 15
-#     self.user = {}
+from PIL import Image
 
-#   def ievaditUserInfo(self):
-#     props = ['klient', 'veltijums', 'augstums', 'garums']
+def bilde():
+  path = "images/mustang.jpeg"
 
-#     for prop in props:
-#       self.user[prop] = input(f'Ievadi ${prop}:')
+  with Image.open(path) as img:
+    print(path, img.format, f"{img.size[0]}x{img.size[1]} {img.mode}")
+    img.show
 
-# class Rekins:
-#   def __init__(self, info):
-#     self.info = info
-
-#   def izdrukat(self):
-#     for attr, value in info.user.__dict__.items():
-#       print(f'{attr}: {value}')
-
-#   def changeProps(self):
-#     self.izdrukat()
-
-#     for attr, value in self.__dict__.items():
-#       self[attr] = input(f'Новое значение для {attr}:')
-
-# info = Info()
-# info.ievaditUserInfo()
-
-# rekins = Rekins(info)
-
-# rekins.izdrukat()
-klient = 'pavel'
-
-def saveDates():
-  import csv
-
-  with open (f'{klient}.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['name', ' veltijums', 'izmeri', 'cena'])
-    print('Saved')
-
-saveDates()
+bilde()
