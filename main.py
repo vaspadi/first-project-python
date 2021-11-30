a1c9bfbd3a2d84a2513ae7eaca5c6e2c40e9b776
Васильев Павел <40562751+vaspadi@users.noreply.github.com>
2021-11-30 06:51:12 +0000

    csv files

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
-person = {
-  'name': 'Pavel',
-  'age': 18,
-  'array': [1,2,3],
-  'object': {
-    'key1': 'value'
-  }
-}
+# class Info:
+#   def __init__(self):
+#     self.PVN = 21
+#     self.darbaSamaksa = 15
+#     self.user = {}
 
-print(person['object']['key1'])
+#   def ievaditUserInfo(self):
+#     props = ['klient', 'veltijums', 'augstums', 'garums']
+
+#     for prop in props:
+#       self.user[prop] = input(f'Ievadi ${prop}:')
+
+# class Rekins:
+#   def __init__(self, info):
+#     self.info = info
+
+#   def izdrukat(self):
+#     for attr, value in info.user.__dict__.items():
+#       print(f'{attr}: {value}')
+
+#   def changeProps(self):
+#     self.izdrukat()
+
+#     for attr, value in self.__dict__.items():
+#       self[attr] = input(f'Новое значение для {attr}:')
+
+# info = Info()
+# info.ievaditUserInfo()
+
+# rekins = Rekins(info)
+
+# rekins.izdrukat()
+klient = 'pavel'
+
+def saveDates():
+  import csv
+
+  with open (f'{klient}.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['name', ' veltijums', 'izmeri', 'cena'])
+    print('Saved')
+
+saveDates()
