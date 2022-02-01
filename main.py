109c4102db8c4385c9ffad48831d927602ddd452
Васильев Павел <40562751+vaspadi@users.noreply.github.com>
2022-02-01 06:44:46 +0000

    create datebase of persons

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from replit import db
 
+def seedDb():
+# Set test data id db
+  for index in range(5):
+    db[f"person{index}"] = {
+      'firstName': f"Name{index}",
+      'secondName': f"SecondName{index}",
+      'number': f"+37{index} 28282828",
+      'age': 18 + index,
+      'isWorker': index % 2 == 0,
+    }
+
 def add(key='', value=''):
   if (key == ''):
     key = input('Adding - Введите ключ: ')
@@ -11,11 +22,16 @@ def add(key='', value=''):
   
   print(f"Поле {key} было добавлено. Значение: {value}")
 
-def delete(key=''):
-  if (key == ''):
+def delete(key=None):
+  if (key == None):
     key = input ('Removing - Введите ключ: ')
 
-  del db[key]
+  if (isinstance(key, list)):
+    for item in key:
+      del db[item]
+
+  else:
+    del db[key]
 
   print(f"Поле {key} было удалено")
 
@@ -23,4 +39,27 @@ def dbLog():
   for key in db:
     print(f"db - {key}: {db[key]}")
 
-dbLog()
+def getKeyByValue(value = ''):
+  if (value == ''):
+    value = input('Key getting - Введите значение')
+
+  for key in db:
+    if (value == db[key]):
+      return key
+
+def findPersonBy(key = '', value = None):
+  if (key == ''):
+    key = input('Finding - Введите ключ: ')
+
+  if (value == None):
+    value = input(f"Finding - Введите значение, для ключа {key}: ")
+
+  result = []
+
+  for dbKey in db:
+    if (value == db[dbKey][key]):
+      result.append(db[dbKey])
+  
+  return result
+
+print(findPersonBy('age', 20))
