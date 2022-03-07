11d57fd1b289bc0224e73f4f87cc8c9be68aded7
Васильев Павел <40562751+vaspadi@users.noreply.github.com>
2022-03-07 07:51:52 +0000

    work with csv files

--- main.py
+++ main.py
@@ -1,127 +1,32 @@
-from replit import db
+import csv
 
-def confirmContinue(operation):
-  confirm = ''
+file = open("pavel.csv")
 
-  while confirm.lower() not in ('n', 'y'):
-    confirm = input(f"{operation} - введите y или n - продолжить или закончить: ")
+lasit_csv = csv.reader(file)
 
-  return confirm == 'y'
+saturs = []
 
-def seedDb():
-  confirm = confirmContinue('Test data generating')
+for rinda in lasit_csv:
+    saturs.append(rinda)
 
-  if (confirm == False): return
+columns = ['Gads', 'Iedzīvotāju skaits']
+rows = [
+  ['1947', '1 716 773'],
+  ['1957', '2 040 978'],
+  ['1967', '2 289 645'],
+  ['1977', '2 477 449'],
+  ['1987', '2 612 068'],
+  ['1997', '2 444 912'],
+  [2007, '2 208 840'],
+  ['2017', '1 950 116']
+]
 
-# Set test data id db
-  for index in range(5):
-    db[f"person{index}"] = {
-      'firstName': f"Name{index}",
-      'secondName': f"SecondName{index}",
-      'number': f"+37{index} 28282828",
-      'age': 18 + index,
-      'isWorker': index % 2 == 0,
-    }
-  
-def add(key='', value=''):
-  confirm = confirmContinue('Adding')
+saturs.append(columns)
+for row in rows:
+    saturs.append(row)
 
-  if (confirm == False): return
+file.close()
 
-  if (key == ''):
-    key = input('Adding - Введите ключ: ')
-
-  if (value == ''):
-    value = input('Adding - Введите значение: ')
-
-  db[key] = value
-  
-  print(f"Поле {key} было добавлено. Значение: {value}")
-
-def delete(key=None):
-
-  if (key == None):
-    confirm = confirmContinue('Removing')
-    if (confirm == False): return
-
-    key = input ('Removing - Введите ключ: ')
-
-  if (isinstance(key, list)):
-    for item in key:
-      if hasattr(db, item): del db[item]
-
-  else:
-    if hasattr(db, key): del db[key]
-
-  print(f"Поле {key} было удалено")
-
-def dbLog():
-  for key in db:
-    print(f"db - {key}: {db[key]}")
-
-def clearDb():
-  for key in db:
-    del db[key]
-
-def getKeyByValue(value = ''):
-  if (value == ''):
-    value = input('Key getting - Введите значение')
-
-  for key in db:
-    if (value == db[key]):
-      return key
-
-def findPersonBy(key = '', value = None):
-  if (key == ''):
-    key = input('Finding - Введите ключ: ')
-
-  if (value == None):
-    value = input(f"Finding - Введите значение, для ключа {key}: ")
-
-  result = []
-
-  for dbKey in db:
-    if (value == db[dbKey][key]):
-      result.append(db[dbKey])
-  
-  return result
-
-def createPerson(person = None, key = None):
-
-  if (person != None and key != None): db[key] = person
-
-  confirm = confirmContinue('Person creating')
-
-  if (confirm == False): return
-
-  dbKey = input('Введите ключ для db: ')
-  firstName = input('Введите Имя: ')
-  secondName = input('Введите Фамилию: ')
-  number = input('Введите номер телефона: ')
-  age = input('Введите возраст: ')
-  isWorker = input('Работает ли он? (y/n): ')
-
-  db[dbKey] = {
-    'firstName': firstName,
-    'secondName': secondName,
-    'number': number,
-    'age': age,
-    'isWorker': isWorker == 'y',
-  }
-
-# print(findPersonBy('age', 20))
-
-# delete(['person0', 'person1'])
-
-# dbLog()
-# 
-
-clearDb()
-
-seedDb()
-
-createPerson()
-
-delete()
-
-dbLog()
+with open('new_csvfails.csv', 'w', encoding="utf-8",newline='') as fails:
+    csvwrite = csv.writer(fails)
+    csvwrite.writerows(saturs)
